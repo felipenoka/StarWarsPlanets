b3dd6d5f363a0931d8da00fc2c494b087140f45c
Luiz Felipe <43940304+felipenoka@users.noreply.github.com>
2019-05-09 00:52:33 -0300

    Melhorias no App
    *Adicionei um Service para a api e requests com o axios;
    *Agora a aplicação está buscando todos os planetas e gerando IDs;

--- src/components/Planet.js
+++ src/components/Planet.js
@@ -1,33 +1,60 @@
 import React, { Component } from 'react';
-import './Planet.css'
+import api from '../service/api';
+import './Planet.css';
 
 class Planet extends Component {
   state = {
-    data: []
-  }
-  componentDidMount() {
-    const url = 'https://swapi.co/api/planets/'
-    fetch(url)
-      .then(response => response.json())
-      .then(response => {
-        this.setState({
-          data: response.results,
-        })
-      })
+    planets: [],
+    err: false
   }
 
-  getPlanet = (event) => {
-    this.forceUpdate()
+  getPlanet = async (event) => {
+    const randomPlanet = (min, max) => {
+      const maximum = Math.floor(max);
+      const minimun = Math.ceil(min);
+      const randomNum = Math.floor(Math.random() * (maximum - minimun + 1)) + minimun;
+
+      // eslint-disable-next-line
+      if (randomNum === randomNum) {
+        return randomNum + 1;
+      }
+      return randomNum;
+    }
+  const Planet = randomPlanet(1, 61);
+  try {
+    const resp = await api.get(`planets/${Planet}`);
+
+    this.setState({
+      planets: [
+        {
+          id: Math.floor(Math.random() * 61),
+          name: resp.data.name,
+          population: resp.data.population,
+          climate: resp.data.climate,
+          terrain: resp.data.terrain,
+          films: resp.data.films,
+        }
+      ],
+      err: false
+    });
+  } catch (err) {
+    this.setState({
+      err: true,
+    })
   }
+}
+
+componentDidMount() {
+  this.getPlanet()
+}
+
   render() {
-    const index = Math.floor(Math.random() * this.state.data.length)
-    const planet = this.state.data[index]
-
+    const { planets } = this.state;
     return (
       <div className="container">
         <div className="planet-box">
-            {planet &&
-            <div>
+            {planets.map(planet => (
+            <div key={planet.id}>
                 <div className="planet-name">
                     <h1>{planet.name}</h1>
                 </div>
@@ -39,7 +66,8 @@ class Planet extends Component {
                     <br />
                     <p className="read text-center">Featured in {planet.films.length} Films</p>
                 </div>
-            </div>}
+            </div>
+            ))}
         </div>
         <div className="mt-5">
             <button className="planet-btn" onClick={this.getPlanet}>NEXT</button>
